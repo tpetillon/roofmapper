1920a903a31423b94377c3a70c7aa699f0dd0e87
tpetillon <tpetillon@gmail.com>
2016-07-12 22:19:26 +0200

    Add Leaflet map, with OSM layer

--- entry.js
+++ entry.js
@@ -1,12 +1,20 @@
 'use strict';
 
-require("./style.css");
 //document.write(require("./content.js"));
 
 var $ = require("jquery");
 var osmAuth = require('osm-auth');
+var L = require('leaflet');
+
+require("leaflet/dist/leaflet.css");
+require("./style.css");
+
+// since leaflet is bundled into the browserify package it won't be able to detect where the images
+// solution is to point it to where you host the the leaflet images yourself
+//L.Icon.Default.imagePath = 'http://cdn.leafletjs.com/leaflet-0.7.3/images';
 
 $("body").append("<button id='authenticate'>Authenticate</button><button id='logout'>Logout</button>");
+$("body").append("<div id='map'></div>");
 
 var auth = osmAuth({
     oauth_consumer_key: 'WLwXbm6XFMG7WrVnE8enIF6GzyefYIN6oUJSxG65',
@@ -42,3 +50,9 @@ document.getElementById('logout').onclick = function() {
     auth.logout();
     console.log("logged out");
 }
+
+var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+var osm = new L.TileLayer(osmUrl, {minZoom: 0, maxZoom: 18, attribution: osmAttrib});
+var map = L.map('map').setView([46.935, 2.780], 7);
+map.addLayer(osm);
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" }
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
         ]
     }
 };
