44500b44c20c8af3cce6feb181af653bff133e74
Thomas Pétillon <tpetillon@gmail.com>
2016-11-20 21:59:33 +0100

    Minimise production build

--- conf/webpack.production.config.js
+++ conf/webpack.production.config.js
@@ -2,4 +2,12 @@ var webpack = require('webpack');
 var Config = require('webpack-config').Config;
  
 module.exports = new Config().extend('conf/webpack.base.config.js').merge({
+    plugins: [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ]
 });
